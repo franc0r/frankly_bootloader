65d50ae124bd41d7d8c788e9ddd6602680289752
Martin Bauernschmitt <martin.bauernschmitt@posteo.de>
2022-11-28 05:46:05 +0000

    Added test for data conversion

--- tests/src/basic_tests/tests.cpp
+++ tests/src/basic_tests/tests.cpp
@@ -13,6 +13,8 @@
 #include <francor/franklyboot/handler.h>
 #include <gtest/gtest.h>
 
+using namespace franklyboot;
+
 /**
  * @brief Test if static values are calculated correctly from the template arguments
  */
@@ -37,18 +39,27 @@ TEST(BasicTests, checkFlashInfoGetFuncions)  // NOLINT
 
 /**
  * @brief Check if 32-bit unsigned int value is packed correctly to the msg buffer
- *
  */
 TEST(BasicTests, convertU32ToMsgData) {
-  // Implement test
-  EXPECT_EQ(true, false);
+  constexpr uint32_t TEST_U32_VALUE = 0xDEADBEEFU;
+  constexpr msg::MsgData EXPECTED_VALUES = {0xEF, 0xBE, 0xAD, 0xDE};
+
+  msg::MsgData msg_data;
+  msg::convertU32ToMsgData(TEST_U32_VALUE, msg_data);
+
+  for (auto idx = 0U; idx < EXPECTED_VALUES.size(); idx++) {
+    EXPECT_EQ(EXPECTED_VALUES[idx], msg_data[idx]);
+  }
 }
 
 /**
  * @brief Check if the msg buffer is correctly converted to a unsigned int 32-bit value
- *
  */
 TEST(BasicTests, convertMsgDataToU32) {
-  // Implement test
-  EXPECT_EQ(true, false);
+  constexpr uint32_t EXPECTED_VALUE = 0xDEADBEEFU;
+  constexpr msg::MsgData TEST_MSG_DATA = {0xEF, 0xBE, 0xAD, 0xDE};
+
+  const uint32_t msg_value = msg::convertMsgDataToU32(TEST_MSG_DATA);
+
+  EXPECT_EQ(msg_value, EXPECTED_VALUE);
 }
